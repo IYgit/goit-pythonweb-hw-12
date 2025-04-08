29456585a48f106d80ccbcc62e11ea74ea55d5ea
i.yanchyk <i.yanchyk@steel.mil.ua>
2025-04-08 22:27:40 +0300

    api, service, repository

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,9 +1,11 @@
 import os
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.engine.url import URL
 from pathlib import Path
 from urllib.parse import quote_plus
+
+from dotenv import load_dotenv
+from pydantic import ConfigDict, EmailStr
+from pydantic_settings import BaseSettings
+from sqlalchemy.engine.url import URL
 
 # Отримуємо шлях до кореня проєкту (hw10)
 project_root = Path(__file__).resolve().parents[2]
@@ -53,3 +55,33 @@ class Config:
 
 
 config = Config
+
+
+class Settings(BaseSettings):
+    DB_URL: str = config.DB_URL
+
+    JWT_SECRET: str = os.getenv("JWT_SECRET")
+    JWT_ALGORITHM: str = "HS256"
+    JWT_EXPIRATION_SECONDS: int = 3600
+
+    MAIL_USERNAME: EmailStr = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD: str = os.getenv("MAIL_PASSWORD")
+    MAIL_FROM: EmailStr = os.getenv("MAIL_FROM")
+    MAIL_PORT: int = os.getenv("MAIL_PORT")
+    MAIL_SERVER: str = os.getenv("MAIL_SERVER")
+    MAIL_FROM_NAME: str = "API Service"
+    MAIL_STARTTLS: bool = False
+    MAIL_SSL_TLS: bool = True
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool = True
+
+    CLOUDINARY_NAME: str = os.getenv("CLOUDINARY_NAME")
+    CLOUDINARY_API_KEY: int = os.getenv("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET: str = os.getenv("CLOUDINARY_API_SECRET")
+
+    model_config = ConfigDict(
+        extra="ignore", env_file=".env", env_file_encoding="utf-8", case_sensitive=True
+    )
+
+
+settings = Settings()
