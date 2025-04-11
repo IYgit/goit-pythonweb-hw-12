f845c48acffa0523f0aab55ec9a15f092ee3b989
i.yanchyk <i.yanchyk@steel.mil.ua>
2025-04-11 17:00:18 +0300

    CORS

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
 from src.api import contacts, utils, auth, users
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Дозволяє всім доменам
+    allow_credentials=True,
+    allow_methods=["*"],  # Дозволяє всім методам
+    allow_headers=["*"],  # Дозволяє всім заголовкам
+)
+
 
 app.include_router(utils.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
